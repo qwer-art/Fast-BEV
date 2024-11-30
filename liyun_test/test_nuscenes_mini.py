b3568ed87635192f251cc514f50b9217df3c2660
liyun <1062428799@qq.com>
2024-12-01 00:22:50 +0800

    生成数据的初步理解完成

--- liyun_test/test_nuscenes_mini.py
+++ liyun_test/test_nuscenes_mini.py
@@ -1,9 +1,9 @@
 from os import path as osp
 import sys
 project_root = osp.dirname(osp.dirname(osp.abspath(__file__)))
-print(f"project_root: {project_root}")
 sys.path.append(project_root)
 from tools.data_converter import nuscenes_converter as nuscenes_converter
+import mmcv
 
 dataset = 'nuscenes'
 version = 'v1.0-mini'
@@ -14,5 +14,11 @@ max_sweeps=10
 
 if __name__ == '__main__':
     ### [1].data_infos
-    nuscenes_converter.create_nuscenes_infos(
-        root_path, info_prefix, version=version, max_sweeps=max_sweeps)
+    # nuscenes_converter.create_nuscenes_infos(
+    #     root_path, info_prefix, version=version, max_sweeps=max_sweeps)
+    info_train_path = osp.join(root_path, f'{info_prefix}_infos_train.pkl')
+    info_val_path = osp.join(root_path, f'{info_prefix}_infos_val.pkl')
+    nuscenes_converter.export_2d_annotation(
+        root_path, info_train_path, version=version)
+    nuscenes_converter.export_2d_annotation(
+        root_path, info_val_path, version=version)
